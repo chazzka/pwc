912837d8d1bb0d9bf2f0962b4a8158d120479fa4
Adam Ulrich <adamo.ulrich@gmail.com>
2024-07-08 09:57:36 +0200

    ch277 done

---
+++ ch-277/lib/ch277.ex
@@ -0,0 +1,23 @@
+defmodule Ch277 do
+  @moduledoc """
+  Documentation for `Ch277`.
+  """
+
+  @doc """
+  count search - it returns a map with items that are present "count" times
+  """
+  def count_search(w, count), do: w |> Enum.frequencies |> Map.filter(fn {_, val} -> val == count end)
+
+
+  @doc """
+  count search - it returns a map with items that are present "count" times
+  """
+  def strong_pair?(p) do
+    case p do
+      [a,b] when 0 < abs(a-b) and abs(a-b) < min(a,b) -> true
+      _ -> false
+    end
+  end
+
+
+end
---
+++ ch-277/test/ch277_test.exs
@@ -0,0 +1,54 @@
+defmodule Ch277Test do
+  use ExUnit.Case
+  doctest Ch277
+
+  test "count_search" do
+    assert ["Perl", "is", "my", "friend"] |> Ch277.count_search(1) == %{"Perl" => 1, "friend" => 1, "is" => 1, "my" => 1}
+  end
+
+  test "ch1_1" do
+    first = ["Perl", "is", "my", "friend"]
+    |> Ch277.count_search(1)
+    second = ["Perl", "and", "Raku", "are", "friend"]
+    |> Ch277.count_search(1)
+
+    assert Map.intersect(first, second) |> Map.values |> Enum.count == 2
+  end
+
+  test "ch1_2" do
+    first = ["Perl", "and", "Python", "are", "very", "similar"]
+    |> Ch277.count_search(1)
+    second = ["Python", "is", "top", "in", "guest", "languages"]
+    |> Ch277.count_search(1)
+
+    assert Map.intersect(first, second) |> Map.values |> Enum.count == 1
+  end
+
+  test "ch1_3" do
+    first = ["Perl", "is", "imperative", "Lisp", "is", "functional"]
+    |> Ch277.count_search(1)
+    second = ["Crystal", "is", "similar", "to", "Ruby"]
+    |> Ch277.count_search(1)
+
+    assert Map.intersect(first, second) |> Map.values |> Enum.count == 0
+  end
+
+  test "strong_pair" do
+    assert Ch277.strong_pair?([2,3]) == true
+    assert Ch277.strong_pair?([3,4]) == true
+    assert Ch277.strong_pair?([3,5]) == true
+    assert Ch277.strong_pair?([4,5]) == true
+    assert Ch277.strong_pair?([5,5]) == false
+  end
+
+  test "ch2_1" do
+    assert [1, 2, 3, 4, 5] |> Combination.combine(2) |> Enum.uniq |> Enum.filter(&(Ch277.strong_pair?(&1))) |> Enum.count == 4
+  end
+
+  test "ch2_2" do
+    assert [5, 7, 1, 7] |> Combination.combine(2) |> Enum.uniq |> Enum.filter(&(Ch277.strong_pair?(&1))) |> Enum.count == 1
+  end
+
+
+
+end
