682b0dd3a34a68caa7f7035fd672f6a4f7537338
Adam Ulrich <adamo.ulrich@gmail.com>
2024-07-22 09:06:31 +0200

    refactor dle hanze

--- ch-279/elix/test/elix_test.exs
+++ ch-279/elix/test/elix_test.exs
@@ -7,19 +7,19 @@ defmodule ElixTest do
   end
 
   test "task1" do
-    assert Enum.zip(["R", "E", "P", "L"], [3, 2, 1, 4])
-    |> Enum.sort(fn {_k1, v1}, {_k2, v2} -> v1 <= v2 end)
-    |> Stream.map(fn {char, _} -> char end)
+    assert Enum.zip([3, 2, 1, 4],["R", "E", "P", "L"])
+    |> Enum.sort
+    |> Stream.map(fn {_, char} -> char end)
     |> Enum.join() == "PERL"
 
-    assert Enum.zip(["A", "U", "R", "K"], [2, 4, 1, 3])
-    |> Enum.sort(fn {_k1, v1}, {_k2, v2} -> v1 <= v2 end)
-    |> Stream.map(fn {char, _} -> char end)
+    assert Enum.zip([2, 4, 1, 3], ["A", "U", "R", "K"])
+    |> Enum.sort
+    |> Stream.map(fn {_, char} -> char end)
     |> Enum.join() == "RAKU"
 
-    assert Enum.zip(["O", "H", "Y", "N", "P", "T"], [5, 4, 2, 6, 1, 3])
-    |> Enum.sort(fn {_k1, v1}, {_k2, v2} -> v1 <= v2 end)
-    |> Stream.map(fn {char, _} -> char end)
+    assert Enum.zip([5, 4, 2, 6, 1, 3], ["O", "H", "Y", "N", "P", "T"])
+    |> Enum.sort
+    |> Stream.map(fn {_, char} -> char end)
     |> Enum.join() == "PYTHON"
   end
 
@@ -27,7 +27,6 @@ defmodule ElixTest do
 
   def count_vowels(something), do: count_vowels(something, ~w(a e i o u A E I O U), 0)
   def count_vowels([char|rest], vowels, count), do: count_vowels(rest, vowels, count + (if char in vowels, do: 1, else: 0))
-  # def count_vowels([char|rest], vowels, count), do: count_vowels(rest, vowels, count)
   def count_vowels([], _, count), do: count
 
   test "count_vowels" do
